0e77a383ec844b9d55dea42e58193297da9970b3
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2023-03-17 14:03:18 +0100

    Initial commit

---
+++ src/main/java/com/example/webshopproject/WebshopprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopprojectApplication.class, args);
+	}
+
+}
